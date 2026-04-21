e6721894a59e55bb28b5b43287e383f31e57b236
Rasmus <rasmus_skriver@yahoo.dk>
2026-04-21 21:30:07 +0200

    test

--- init.lua
+++ init.lua
@@ -87,11 +87,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:h nvim_create_user_command()` and `:h user-commands`
 
 -- Create a command `:GitBlameLine` that print the git blame for the current line
-vim.api.nvim_create_user_command('GitBlameLine', function()
-  local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
-  local filename = vim.api.nvim_buf_get_name(0)
-  print(vim.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }):wait().stdout)
-end, { desc = 'Print the git blame for the current line' })
+-- vim.api.nvim_create_user_command('GitBlameLine', function()
+--   local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
+--   local filename = vim.api.nvim_buf_get_name(0)
+--   print(vim.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }):wait().stdout)
+-- end, { desc = 'Print the git blame for the current line' })
 
 -- PLUGINS
 --
